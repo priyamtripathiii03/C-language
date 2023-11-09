204261ed5c4f4ff155b16a12fcd0c209471b2b33
Priyam Tripathi <149374259+priyamtripathiii03@users.noreply.github.com>
2023-11-09 15:44:58 +0530

    Add files via upload

---
+++ loop/While loop/1 to n.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+
+
+int a=1;
+int b;
+printf("Enter the value : ");
+scanf("%d",&b);
+while (a<=10)
+{
+	printf("%d\t",a);
+	a++;
+}
+
+
+
+
+}
---
+++ loop/While loop/while loop n to 1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	
+	int a=1;
+    int n;
+    printf("enter the value of n :");
+    scanf("%d",&n);
+	
+	while(n>=a)
+	{
+		printf("%d\t",n);
+		n--;
+	}
+}

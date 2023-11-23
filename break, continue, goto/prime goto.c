b53d1d2378241f84aa77d40bc0e3daf1b0b387a5
Priyam Tripathi <149374259+priyamtripathiii03@users.noreply.github.com>
2023-11-23 16:11:17 +0530

    Update prime goto.c

--- break, continue, goto/prime goto.c
+++ break, continue, goto/prime goto.c
@@ -2,17 +2,17 @@
 
 main()
 {
-	int start = 2;
-	int end=50,sum=17;
+	int start = 1;
+	int end=50,sum=0;
 	
 
-	printf("2 3 5 7 ");
+	
 	
 	repeat :
 		if(start <= end)
 		 {
 		 		start++;
-		    if (start%2!=0 && start%3!=0 && start%5!=0 && start%7!=0)
+		    if (start%2!=0 && start%3!=0 && start%5!=0 && start%7!=0 || start==2 ||  start==3 ||  start==5 ||  start==7)
 		       {
 			     printf("%d ",start);
 			     	sum+=start;	

6e6cf249179facc75ffa743795d73705d4bfede2
Priyam Tripathi <149374259+priyamtripathiii03@users.noreply.github.com>
2023-11-28 16:10:11 +0530

    Add files via upload

---
+++ Pattern/p22.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+main()
+{
+
+	char i,j,count='A';
+	
+	for(i='A'; i<='E'; i++)
+	{
+		for(j='A'; j<=i; j++)
+		{
+			printf("%c",count);
+			count++;
+		}
+		printf("\n");
+	}
+
+}

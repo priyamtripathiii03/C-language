cd53efa73aacfc984633f1710d92ff3c4116de60
Priyam Tripathi <149374259+priyamtripathiii03@users.noreply.github.com>
2023-11-30 16:51:24 +0530

    Add files via upload

---
+++ pattern with space/V.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+main()
+{
+    int i,j;
+    for(i=1; i<=7; i++)
+    {
+        for(j=1; j<=5; j++)
+        {
+            if((i==7 && j==1)||(i==7 && j==5)||(i==6 && j==1)||(i==6 && j==5))
+            {
+                printf("  ");
+            }
+            else if(j==1 || j==5 || (i==7 && j==3) || (i==6 && j==2) || (i==6 && j==4))
+            {
+                printf("* ");
+            }
+            else
+            {
+                printf("  ");        
+            }
+        }
+             printf("\n");
+    }
+
+}

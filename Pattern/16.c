b6d0d91097de3ba0012dde7a4e456edc7e60f266
Priyam Tripathi <149374259+priyamtripathiii03@users.noreply.github.com>
2023-12-01 15:35:13 +0530

    Update 16.c

--- Pattern/16.c
+++ Pattern/16.c
@@ -4,13 +4,12 @@ main()
 {
     int i,j,k=1;
 	
-	for(i=1; i<=5; i++)
+	for(i=5; i>=1; i--)
 	{
-		for(j=5; j>=i; j--)
+		for(j=i; j>=1; j--)
 		{
-			printf("%d",k%2);
-			k++;
-		}
-		printf("\n");
-	}
+			printf("%d",j%2);
+	     }
+	     printf("\n");
+    }
 }

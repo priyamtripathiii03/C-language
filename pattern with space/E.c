9ab85c9b0ea1c5ece46540be1799fe05ad888e4c
Priyam Tripathi <149374259+priyamtripathiii03@users.noreply.github.com>
2024-02-26 18:34:49 +0530

    Add files via upload

--- pattern with space/E.c
+++ pattern with space/E.c
@@ -2,22 +2,26 @@
 
 main()
 {
-    int i,j;
-    for(i=1; i<=7; i++)
-    {
-        for(j=1; j<=5; j++)
-        {
-            if(i==1 || i==4|| i==7 || j==1)
-            {
-                printf("* ");
-            }
-            else
-            {
-                printf("  ");
-            }
-        }
-            printf("\n");
-    }
-
+	int i,j;
+	
+	for(i=1;i<=7;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			if(i==1 || j==1 || i==4 || i==7)
+			{
+			 printf("*");	
+			}
+			else
+			{
+				printf(" ");
+				}	
+		}
+		printf("\n");
+	}
+	
+	
+	
+	
+	
 }
-
